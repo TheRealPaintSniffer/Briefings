a41a4a2822fd1b75b9d8651b84301ae02faed8f4
TheRealPaintSniffer <151170956+TheRealPaintSniffer@users.noreply.github.com>
2024-01-13 19:42:29 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -125,20 +125,28 @@ export default {
           "mech": "YAGUARAÇÚ MK I"
         },
         {
-          "callsign": "Peel_Placeholder",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
+          "callsign": "BISCUIT",
+          "alias": "IGNATZ STAINS",
+          "code": "Stains※.Ignatz:37df03f9-1efe-4000-8c7a-249d8e84c076//NDL-C-IOTA-BREATH",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "_"
+          "mech": "AGGRESSIVE PEACEKEEPER"
         },
         {
-          "callsign": "_",
-          "alias": '_',
+          "callsign": "BEWG",
+          "alias": 'Prys Reierson',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "_"
+          "mech": "BABY"
+        },
+         {
+          "callsign": "BEWG",
+          "alias": 'Prys Reierson',
+          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "BABY"
         },
       ],
       "header": {
